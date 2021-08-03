86080bfb62a4aab2dabdfbf6b941ba4a071e97a8
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-08-03 14:22:32 -0400

    Update Brainy_Train_Unet.py

--- training_scripts/Brainy_Train_Unet.py
+++ training_scripts/Brainy_Train_Unet.py
@@ -6,15 +6,15 @@ import yaml
 
 def main(config):
     # Set parameters
-    n_classes = config['dataset']['n_classes']
-    batch_size = config['dataset']['train']['batch_size']
-    v =  config['dataset']['train']['volume_shape']
-    b = config['dataset']['train']['block_shape']
+    n_classes = config['n_classes']
+    batch_size = config['dataset_train']['batch_size']
+    v =  config['dataset_train']['volume_shape']
+    b = config['dataset_train']['block_shape']
     volume_shape = (v, v, v)
     block_shape = (b, b, b)
     n_epochs = config['train']['epoch']
     
-    if config['dataset']['train']['name'] == 'sample_MGH':
+    if config['dataset_train']['name'] == 'sample_MGH':
         #Load sample Data--- inputs and labels 
         csv_of_filepaths = nobrainer.utils.get_data()
         filepaths = nobrainer.io.read_csv(csv_of_filepaths)
@@ -52,9 +52,9 @@ def main(config):
         volume_shape=volume_shape,
         block_shape=block_shape,
         n_epochs=n_epochs,
-        augment=config['dataset']['train']['augment'],
-        shuffle_buffer_size=config['dataset']['train']['shuffle_buffer_size'],
-        num_parallel_calls=config['dataset']['train']['num_parallel_calls'],
+        augment=config['dataset_train']['augment'],
+        shuffle_buffer_size=config['dataset_train']['shuffle_buffer_size'],
+        num_parallel_calls=config['dataset_train']['num_parallel_calls'],
     )
     
     dataset_evaluate = nobrainer.dataset.get_dataset(
@@ -64,9 +64,9 @@ def main(config):
         volume_shape=volume_shape,
         block_shape=block_shape,
         n_epochs=1,
-        augment=config['dataset']['test']['augment'],
-        shuffle_buffer_size=config['dataset']['test']['shuffle_buffer_size'],
-        num_parallel_calls=config['dataset']['test']['num_parallel_calls'],
+        augment=config['dataset_test']['augment'],
+        shuffle_buffer_size=config['dataset_test']['shuffle_buffer_size'],
+        num_parallel_calls=config['dataset_test']['num_parallel_calls'],
     )
     
     # Compile model
