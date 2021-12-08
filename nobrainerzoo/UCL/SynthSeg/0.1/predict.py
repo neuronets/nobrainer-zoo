cb4d48f8cafef0176bf62d71214e2f9214c057da
Hoda Rajaei <rajaei.hoda@gmail.com>
2021-12-07 16:14:04 -0800

    update SynthSeg

--- nobrainerzoo/UCL/SynthSeg/0.1/predict.py
+++ nobrainerzoo/UCL/SynthSeg/0.1/predict.py
@@ -1,3 +1,4 @@
+# This code is adapted from SynthSeg_predict.py to be compatible for Nobrainer-zoo
 """This script enables to launch predictions with SynthSeg from the terminal."""
 
 # print information
@@ -33,7 +34,7 @@ parser.add_argument("--vol", type=str, default=None, dest="path_volumes",
                     help="(optional) Output CSV file with volumes for all structures and subjects.")
 
 # parameters
-parser.add_argument("--crop", type=int, default=None, dest="cropping",
+parser.add_argument("--crop", nargs='+', type=int, default=192, dest="cropping",
                     help="(optional) Size of 3D patches to analyse. Default is 192.")
 parser.add_argument("--threads", type=int, default=1, dest="threads",
                     help="(optional) Number of cores to be used. Default is 1.")
@@ -54,14 +55,12 @@ tf.config.threading.set_intra_op_parallelism_threads(args['threads'])
 del args['threads']
 
 # default parameters
-path_label_list = os.path.join(model_repo, 'data/labels_classes_priors/segmentation_labels.npy')
-path_names_list = os.path.join(model_repo, 'data/labels_classes_priors/segmentation_names.npy')
-path_topology_classes = os.path.join(model_repo, 'data/labels_classes_priors/topological_classes.npy')
-path_model = os.path.join(model_repo, 'models/SynthSeg.h5')
-args['segmentation_label_list'] = path_label_list
-args['segmentation_names_list'] = path_names_list
-args['topology_classes'] = path_topology_classes
-args['path_model'] = path_model
+args['segmentation_labels'] = os.path.join(model_repo, 'data/labels_classes_priors/segmentation_labels.npy')
+args['n_neutral_labels'] = 18
+args['segmentation_label_names'] = os.path.join(model_repo, 'data/labels_classes_priors/segmentation_names.npy')
+args['topology_classes'] = os.path.join(model_repo, 'data/labels_classes_priors/topological_classes.npy')
+args['path_model'] = os.path.join(model_repo, 'models/SynthSeg.h5')
+args['padding'] = args['cropping']
 
 # call predict
 predict(**args)
