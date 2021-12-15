4660b7973cf4919ce0a2d01f1ec06aea8f4122b8
Sean I Young <siy0@kale.nmr.mgh.harvard.edu>
2021-12-15 12:19:54 -0500

    parcnet without pretrained model file

---
+++ nobrainerzoo/lcn/parcnet/parcnet_demo.py
@@ -0,0 +1,16 @@
+import torch
+import punet
+
+# load a test parcellation image
+item = torch.load('item.pt')
+
+# instantiate the pretrained model
+image = item[0][None]
+model = punet.unet2d_320_dktatlas_positional_20()
+
+# inference
+output = model(image)[0].argmax(0)
+
+# pixel accuracy
+gtruth = item[1].float().argmax(0)
+acc = torch.mean((gtruth == output).float())
