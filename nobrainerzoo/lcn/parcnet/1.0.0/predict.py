c615f550eb5ad4afc3e905e09742cad1fbffa001
Sean I Young <siy0@kale.nmr.mgh.harvard.edu>
2022-01-24 22:22:49 -0500

    predict.py takes in input_dir and output_dir as arguments and automatically processes everything in input_dir

--- nobrainerzoo/lcn/parcnet/1.0.0/predict.py
+++ nobrainerzoo/lcn/parcnet/1.0.0/predict.py
@@ -1,16 +1,50 @@
+from argparse import ArgumentParser
+import os
 import torch
 import punet
+import parc
 
-# load a test parcellation image
-item = torch.load('item.pt')
+# print information
+print('\n')
+print('ParcNet cortical parcellation')
+print('\n')
 
-# instantiate the pretrained model
-image = item[0][None]
-model = punet.unet2d_320_dktatlas_positional_20_1_0_0()
+# parse arguments
+parser = ArgumentParser()
+parser.add_argument("path_images", type=str, help="images to super-resolve / synthesize. Can be the path to a single image or to a folder")
+parser.add_argument("path_predictions", type=str,
+                    help="path where to save the synthetic 1mm MP-RAGEs. Must be the same type "
+                         "as path_images (path to a single image or to a folder)")
+parser.add_argument("--cpu", action="store_true", help="enforce running with CPU rather than GPU.")
+args = vars(parser.parse_args())
 
-# inference
-output = model(image)[0].argmax(0)
+# enforce CPU processing if necessary
+if args['cpu']:
+    print('using CPU, hiding all CUDA_VISIBLE_DEVICES')
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-# pixel accuracy
-gtruth = item[1].float().argmax(0)
-acc = torch.mean((gtruth == output).float())
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Prepare list of images to process
+path_images = os.path.abspath(args['path_images'])
+basename = os.path.basename(path_images)
+path_predictions = os.path.abspath(args['path_predictions'])
+
+dataset = parc.PARC(root=path_images, subset='.', split=None, mode='image', labels='', in_channels=3, num_classes=32, labeled=False)
+model = punet.unet2d_320_dktatlas_positional_20_1_0_0().to(device)
+percentile = 0.02
+
+print('Found %d subjects' % len(dataset))
+for idx in range(len(dataset)):
+    print('  Working on subject %d ' % (idx+1))
+    img = dataset.__getitem__(idx)[0].to(device)
+    minvals = torch.kthvalue(img.flatten(1),round(img.flatten(1).shape[1]*(percentile-0)),dim=1)[0][:,None,None]
+    maxvals = torch.kthvalue(img.flatten(1),round(img.flatten(1).shape[1]*(1-percentile)),dim=1)[0][:,None,None]
+    img = torch.min(torch.max(img,minvals),maxvals)
+    img = (img - img.flatten(1).mean(1).reshape(3,1,1)) * (1 / img.flatten(1).std(1).reshape(3,1,1))
+
+    dataset.save_output(path_predictions, [model(img[None]).detach().cpu()[0].argmax(0, keepdims=True)], [idx])
+
+print(' ')
+print('All done!')
+print(' ')
+
---
+++ nobrainerzoo/lcn/parcnet/1.0.0/readme.txt
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------
+Positionally encoded U-Net for robust cortical parcellation (v1.0.0)
+usage: python predict.py <path_images> <path_predictions>
+
+path_images is a directory containing FS subjects, each of which has a
+surf directory with parameterizations inflated.H.mgz, surf.mgz,
+curv.mgz
+
+path_predictions is a directory to which the parcellation predictions
+are stored.
+--------------------------------------------------------------------
