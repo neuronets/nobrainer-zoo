f76a58563098e8043d691d97581fac22f71ef7d7
Hoda Rajaei <rajaei.hoda@gmail.com>
2022-03-07 14:35:56 -0800

    packaging files added.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup #,find_packages
 
-setup(
-    name="nobrainer-zoo",
-    version='0.0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'Click',
-        'pyyaml'
-    ],
-    entry_points='''
-        [console_scripts]
-        nobrainer-zoo=nobrainerzoo.cli:cli
-    ''',
-)
+setup(name="nobrainer-zoo")
+
+# setup(
+#     name="nobrainer-zoo",
+#     version='0.0.1',
+#     packages=find_packages(),
+#     include_package_data=True,
+#     install_requires=[
+#         'Click',
+#         'pyyaml'
+#     ],
+#     entry_points='''
+#         [console_scripts]
+#         nobrainer-zoo=nobrainerzoo.cli:cli
+#     ''',
+# )
